b795b7ab1a141e5e59032d71c6a0b70ea720bf0a
divbok <dbokadia@gmail.com>
2022-09-08 18:02:18 -0500

    Ruby Programming Assignment 1

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -4,28 +4,34 @@
 
 def sum(arr)
   # YOUR CODE HERE
+  arr.sum
 end
 
 def max_2_sum(arr)
   # YOUR CODE HERE
+  arr.max(2).sum
 end
 
 def sum_to_n?(arr, number)
   # YOUR CODE HERE
+  arr.combination(2).select { |c| c.sum == number }.any?
 end
 
 # Part 2
 
 def hello(name)
   # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant?(string)
   # YOUR CODE HERE
+  /^[^aeiou\W]/i.match(string) == nil ? false : true
 end
 
 def binary_multiple_of_4?(string)
   # YOUR CODE HERE
+  /^(0*1[01]*00|0)$/.match(string) == nil ? false : true
 end
 
 # Part 3
@@ -33,4 +39,17 @@ end
 # Object representing a book
 class BookInStock
   # YOUR CODE HERE
+  attr_accessor :isbn,:price
+  
+  def initialize(isbn,price)
+    if isbn.empty? || price <=0 
+      raise ArgumentError
+    end
+    @isbn = isbn
+    @price = price
+  end
+
+  def price_as_string
+    "$%0.2f"%price
+  end
 end
